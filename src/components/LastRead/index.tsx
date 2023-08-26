f35450022ef32b7ed6bf9949eac89778c66b9334
Luiz Henrique <7henrique18@gmail.com>
2023-08-26 11:44:45 -0300

    feat: empty lastRead state

--- src/components/LastRead/index.tsx
+++ src/components/LastRead/index.tsx
@@ -1,4 +1,4 @@
-import { CaretRight } from '@phosphor-icons/react'
+import { Binoculars, Book, CaretRight, Folder } from '@phosphor-icons/react'
 import Link from 'next/link'
 import { ReadCard, ReadCardSkeleton } from '../ReadCard'
 import { useQuery } from 'react-query'
@@ -10,7 +10,7 @@ export const LastRead = () => {
     async () => await api.getLastRead(),
   )
 
-  if (isLoading || !data) {
+  if (isLoading) {
     return (
       <div>
         <div className="mb-5 flex items-center justify-between">
@@ -25,6 +25,33 @@ export const LastRead = () => {
         </div>
 
         <ReadCardSkeleton />
+      </div>
+    )
+  }
+
+  if (!data) {
+    return (
+      <div>
+        <div className="mb-5 flex items-center justify-between">
+          <h3 className="text-gray-100">Sua última leitura</h3>
+
+          <Link
+            className="flex items-center gap-2 text-purple-100"
+            href="/profile"
+          >
+            Ver todas <CaretRight className="fill-purple-100" weight="bold" />
+          </Link>
+        </div>
+
+        <Link href="/explore">
+          <div className="hover: flex w-full cursor-pointer flex-col  items-center justify-center gap-2 rounded-lg border-2  border-gray-500 bg-gray-600 p-6 transition-all">
+            <Book width={64} height={64} className="fill-gray-400" />
+
+            <p className="text-sm text-gray-400">
+              Nenhum livro foi lido recentemente
+            </p>
+          </div>
+        </Link>
       </div>
     )
   }
