e60bfe034e96edc2e89cd52d5e76674fd10ecf0e
Luiz Henrique <7henrique18@gmail.com>
2023-08-30 19:36:21 -0300

    feat: setup NextNProgress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import { Nunito } from 'next/font/google'
 import { SessionProvider } from 'next-auth/react'
 import { QueryClientProvider, QueryClient } from 'react-query'
+import NextNProgress from 'nextjs-progressbar'
 
 const nunito = Nunito({ subsets: ['latin'], weight: ['400', '700'] })
 export const queryClient = new QueryClient()
@@ -12,6 +13,14 @@ export default function App({ Component, pageProps }: AppProps) {
     <SessionProvider>
       <QueryClientProvider client={queryClient}>
         <main className={nunito.className}>
+          <NextNProgress
+            color="#50B2C0"
+            startPosition={0.3}
+            stopDelayMs={200}
+            height={3}
+            showOnShallow={true}
+          />
+
           <Component {...pageProps} />
         </main>
       </QueryClientProvider>
