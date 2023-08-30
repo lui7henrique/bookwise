2d33cc0bec34e3b4a5753f67a652ffefef58e5b1
Luiz Henrique <7henrique18@gmail.com>
2023-08-30 20:03:31 -0300

    feat: setup favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.svg" sizes="any" />
+      </Head>
 
       <body className="overflow-x-hidden bg-gray-800 text-gray-200">
         <Main />
